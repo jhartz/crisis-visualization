dcaec7023a047bb26b2fa04e02151462aa800ffb
jeannforbes <jxf2190@rit.edu>
2016-03-10 11:31:46 -0500

    Preliminary bubble
    It’s there and not at all square.

--- Prototype_v1/scripts/main.js
+++ Prototype_v1/scripts/main.js
@@ -1,15 +1,28 @@
-       //MAP  - Specify bubbles, arcs & config in here
-      var map = new Datamap({
-        scope: 'world',
-        element: document.getElementById('worldMap'),
-        projection: 'mercator',
-        height: 500,
-        fills: {
-          defaultFill: '#FFF',
-        },
-        geographyConfig: {
-            borderColor: '#000',
-            highlightFillColor: '#333',
-            highlightBorderColor: '#666'
-        }
-      });
+//MAP  - Specify bubbles, arcs & config in here
+var map = new Datamap({
+  scope: 'world',
+  element: document.getElementById('worldMap'),
+  projection: 'mercator',
+  height: 500,
+  fills: {
+    defaultFill: '#FFF',
+    Facebook: '#00F'
+  },
+  geographyConfig: {
+      borderColor: '#000',
+      highlightFillColor: '#333',
+      highlightBorderColor: '#666'
+  }
+});
+
+map.bubbles([{
+        name: 'Test 1',
+        radius: 50,
+        fillKey: 'Facebook',
+        latitude: 11.415,
+        longitude: 105.1619,
+    }], {
+      popupTemplate: function(geo, data) {
+        return '<div class="hoverinfo">'+data.name+''
+      }
+    });
