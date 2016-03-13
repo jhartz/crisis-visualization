bc91e7bb91bd3d5c8d58730ead51a33b570fb288
Jake Hartz <jhartz@outlook.com>
2016-03-13 14:49:57 -0400

    Worked on code style and CSS
    Moved to flexbox layout to try and allow map to resize with window size

--- Prototype_v1/scripts/main.js
+++ Prototype_v1/scripts/main.js
@@ -1,32 +1,40 @@
 //MAP  - Specify bubbles, arcs & config in here
 var map = new Datamap({
-  scope: 'world',
-  element: document.getElementById('worldMap'),
-  projection: 'mercator',
-  height: 500,
-  fills: {
-    defaultFill: '#FFF',
-    facebook: '#00F',
-    twitter: '#0F0',
-    website: '#F00'
-  },
-  geographyConfig: {
-      borderColor: '#000',
-      highlightFillColor: '#333',
-      highlightBorderColor: '#666'
-  }
+    scope: "world",
+    element: document.getElementById("worldMap"),
+    projection: "mercator",
+    responsive: true,
+    fills: {
+        defaultFill: "#FFF",
+        facebook: "#00F",
+        twitter: "#0F0",
+        website: "#F00"
+    },
+    geographyConfig: {
+        borderColor: "#000",
+        highlightFillColor: "#333",
+        highlightBorderColor: "#666"
+    }
 });
 
-function addBubbles(bubbles){
-  map.bubbles(bubbles, {
-    popupTemplate: function(geo, data){
-      return '<div class="hoverinfo">'+data.name.replace(/\n/g, "<br>")
-    }
-  });
+function addBubbles(bubbles) {
+    map.bubbles(bubbles, {
+        popupTemplate: function (geo, data) {
+            return (
+                '<div class="hoverinfo">' +
+                data.name.replace(/\n/g, '<br>') +
+                '</div>'
+            );
+        }
+    });
 }
 
 
 // Read the local data in ../social.json
 window.addEventListener("load", function (event) {
     readJSON("../social.json");
+}, false);
+
+window.addEventListener("resize", function (event) {
+    map.resize();
 }, false);
