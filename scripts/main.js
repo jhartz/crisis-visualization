5805e17e8fb1ae17f58233444898988cc0e4ec84
Jake Hartz <jhartz@outlook.com>
2016-05-10 11:49:41 -0400

    Changed URL

--- scripts/main.js
+++ scripts/main.js
@@ -2,6 +2,6 @@ window.addEventListener("load", function (event) {
     // Initialize the D3 DataMap
     initMap(SOCIAL_MEDIA_CONFIG);
 
-    // Read the social media data in ../social.json
-    readJSON("../social.json", SOCIAL_MEDIA_CONFIG);
+    // Read the social media data
+    readJSON("data/social.json", SOCIAL_MEDIA_CONFIG);
 }, false);
